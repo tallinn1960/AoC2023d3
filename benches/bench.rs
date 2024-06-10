77d36c97a585c3bb6e11bc0b26b60054b3c8bcfc
Olaf Schlüter <tallinn1960@yahoo.de>
2024-06-10 13:05:02 +0200

    bench for both parts

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,26 +1,27 @@
-use std::io::BufReader;
+use std::{fs::File, io::{BufReader, Read}};
 
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use p3::{p1,  p2};
 
-static INPUT: &str = "467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..";
 
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("p1", |b| {
-        b.iter(|| p3::p1(&mut BufReader::new(INPUT.as_bytes())))
-    });
-    c.bench_function("yt::p1", |b| {
-        b.iter(|| p3::yt::p1(&mut BufReader::new(INPUT.as_bytes())))
-    });
+fn bench_p1(c: &mut Criterion) {
+    c.bench_function("p1", |b| b.iter_batched(|| {
+        let mut f = File::open("input.txt").expect("can't open file");
+        let mut buf = String::new();
+        f.read_to_string(&mut buf).expect("can't read file");
+        buf
+    }, | f| p1(&mut BufReader::new(f.as_bytes())), BatchSize::SmallInput));
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn bench_p2(c: &mut Criterion) {
+    c.bench_function("p2", |b| b.iter_batched(|| {
+        let mut f = File::open("input.txt").expect("can't open file");
+        let mut buf = String::new();
+        f.read_to_string(&mut buf).expect("can't read file");
+        buf
+    }, |f| p2(&mut BufReader::new(f.as_bytes())), BatchSize::SmallInput));
+}
+
+
+criterion_group!(benches, bench_p1, bench_p2);
 criterion_main!(benches);
